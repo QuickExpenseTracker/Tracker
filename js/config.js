bbf4ca5edded99a1291de93724485ca624c78778
QuickExpenseTracker <tiktokkimkc773@gmail.com>
2026-05-12 14:12:14 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+// Configuration for Firebase and Supabase
+export const CONFIG = {
+    FIREBASE: {
+        apiKey: "AIzaSyBKP7aWUNgIVvLeCDVzDkmug48zqtaGz5g",
+        authDomain: "expense-tracker-bef77.firebaseapp.com",
+        projectId: "expense-tracker-bef77",
+        storageBucket: "expense-tracker-bef77.firebasestorage.app",
+        messagingSenderId: "476624625638",
+        appId: "1:476624625638:web:8419541f6a6f9cf5b94027"
+    },
+    SUPABASE: {
+        URL: "https://urgdqzvsjewkhnpfhtup.supabase.co",
+        ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyZ2RxenZzamV3a2hucGZodHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1NTQxNjIsImV4cCI6MjA5NDEzMDE2Mn0.pxuusCSrTpPXYdnKfL7L9dTCOQFOaLQwrbwAeJcIp3g",
+        SERVICE_ROLE: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyZ2RxenZzamV3a2hucGZodHVwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODU1NDE2MiwiZXhwIjoyMDk0MTMwMTYyfQ.-rTgcaR8PbWQUGrL8R0uek7EK6b--9OpLmf5gyiIbOY"
+    }
+};
